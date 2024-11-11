e0702ca6aaba6aeb1ee1cbeefe86d9961e29744b
Afrae <afrae.chakkaf@gmail.com>
2024-11-11 20:33:14 +0100

    Realizamos modificaciones para que funcione con importaciones e importaciones de tipo ES6

--- src/config/env.js
+++ src/config/env.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
-const {get} =require('env-var');
-const envs={
-    PORT: get('PORT').required().asPortNumber(),
-    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString()
+// require('dotenv').config();
+import env from 'dotenv'
+
+
+// const {get} =require('env-var');
+import envvar from 'env-var';
+
+env.config();
+
+export const envs={
+    PORT: envvar.get('PORT').required().asPortNumber(),
+    PUBLIC_PATH: envvar.get('PUBLIC_PATH').default('public').asString()
 }
 
-module.exports = {
-    envs
-}
