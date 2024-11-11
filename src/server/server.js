806a22ec83bf6fb500a4826444bc4c6156ceae7c
Afrae <afrae.chakkaf@gmail.com>
2024-11-11 14:00:59 +0100

    Realizamos un servidor web donde servimos una web tipo red social, como la del pajarito

---
+++ src/server/server.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const path= require('path');
+
+const startServer = (options)=>{
+    const{port,public_path='public'}=options
+    console.log(port)
+    const app=express()
+
+    //midlewares
+    app.use(express.static(public_path))
+    app.get('*', (req,res)=>{
+        const indexPath =path.join(__dirname +`../../../${public_path}/index.html`)
+        res.sendFile(indexPath)
+    })
+    app.listen(port, ()=>{
+        console.log('Escuchando en el puerto '+port)
+    })
+}
+
+
+module.exports={
+    startServer
+}
